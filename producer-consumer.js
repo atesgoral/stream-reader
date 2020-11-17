1148934e7ffaddd21e35c38c97cb12a1cd98f3a9
Ates Goral <ates.goral@shopify.com>
2020-11-17 18:58:26 -0500

    Stashing changes

--- producer-consumer.js
+++ producer-consumer.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /** Meant as documentation of the chunk archetype, but can also be used as a base class */
 class Chunk {
   /**
@@ -74,14 +75,113 @@ class ProducerConsumer { // -> +Broker
       if (this.pendingConsumer) {
         this._consumePending();
       }
+=======
+class ProducerConsumer {
+  constructor() {
+    this.buffer = [];
+    this.producerQueue = [];
+    this.producerQueueResolver = null;
+  }
+
+  queueRead(count, resolve) {
+    console.log('queueRead', count);
+
+    this.readQueue.push({ count, resolve });
+
+    if (this.readQueueResolver) {
+      console.log('resolving read queue');
+      this.readQueueResolver();
+      this.readQueueResolver = null;
+    }
+  }
+
+  readQueued() {
+    console.log('readQueued');
+
+    return new Promise((resolve, reject) => {
+      // if (this.readQueue.length) {
+      //   console.log('resolving');
+      //   resolve();
+      // } else {
+        console.log('setting resolver');
+        this.readQueueResolver = resolve;
+      // }
+    });
+  }
+
+  async depleteChunk(newChunk) {
+    console.log('depleteChunk');
+
+    let chunk = this.buffer
+      ? Buffer.concat([ this.buffer, newChunk ])
+      : newChunk;
+    let chunkStart = 0;
+
+    this.buffer = null;
+
+    while (chunkStart < chunk.length) {
+      if (!this.readQueue.length) {
+        console.log('awaiting queue');
+        await this.readQueued();
+      }
+
+      console.log('got queued reads', this.readQueue.length);
+
+      const read = this.readQueue.shift();
+
+      const haveEnoughBytesInChunk = read.count <= chunk.length;
+
+      if (haveEnoughBytesInChunk) {
+        console.log('have enough bytes');
+        read.resolve(chunk.slice(chunkStart, chunkStart += read.count));
+      } else {
+        console.log('not enough bytes');
+        this.buffer = chunk;
+        break;
+      }
+    }
+
+    return Promise.resolve();
+  }
+
+  _write(chunk, encoding, callback) {
+    console.log('_write', chunk.length);
+
+    this.depleteChunk(chunk)
+      .then(callback)
+      .catch(callback);
+  }
+
+  // @todo implement _writev?
+
+  // @todo implement _destroy
+
+  read(count) {
+    return new Promise((resolve, reject) => {
+      this.queueRead(count, resolve); // @todo return Promise from queueRead instead?
+    });
+  }
+
+  write(chunk, enmcoding) {
+    return new Promise((resolve, reject) => {
+      this._write(chunk, encoding, resolve);
+>>>>>>> 5e094d0... Stashing changes
     });
   }
 
   // @todo to signal no need to deplete latest chunk
   // pass thru?
+<<<<<<< HEAD
   // end() {
 
   // }
 }
 
 module.exports = ProducerConsumer;
+=======
+  end() {
+
+  }
+
+}
+>>>>>>> 5e094d0... Stashing changes
